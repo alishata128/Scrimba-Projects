2046f5e0aefdd743b137464c4a4ea8e09bd533cc
Ali Shata <alishata128@gmail.com>
2022-09-18 21:15:19 +0200

    Delete projects/ColorModes directory

--- projects/ColorModes/script.js
+++
@@ -1,8 +0,0 @@
-const colorModes = document.getElementById("color-modes");
-let chars = [];
-let colors = ["#b58900", "#cb4b16", "#dc322f", "#d33682", "#268bd2"];
-
-for (let i = 0; i <= colorModes.textContent.length; i++) {
-  chars.push(colorModes.textContent.charAt(i));
-}
-console.log("here ", chars[1]);
